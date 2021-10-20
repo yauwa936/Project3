a6076ccfcaeb969904b7d3b5317defad6dae993c
Ananthi <31553747+ananthigok@users.noreply.github.com>
2021-10-20 22:48:27 +1100

    initial supply and mint added

--- contract/createProjectToken/CreateProjectToken.sol
+++ contract/createProjectToken/CreateProjectToken.sol
@@ -1,19 +1,29 @@
 // SPDX-License-Identifier: MIT
+/**
+ * essential function for ERC20 token 
+totalSupply
+balanceOf
+allowance
+transfer
+approve
+transferFrom
+**/
+
 pragma solidity ^0.8.0;
 import "./TokenERC20.sol";
 
 contract CreateProjectToken {
     IERC20[] public erc20s;
-    GnosisSafe public safe; 
 
     function createProjectToken(
         string memory _name,
         string memory _symbol,
-        uint256 _totalSupply
+        uint256 _totalSupply,
+        uint initial_supply
     ) external {
         IERC20 erc20 = new TokenERC20(msg.sender, _name, _symbol, _totalSupply);
         erc20s.push(erc20);
-        
+        mint(msg.sender, initial_supply);
     }
 
     function getProjectTokenAddress() external view returns (IERC20[] memory) {
