dc63d6cba2158e611a87fa6b888a9650e4305c31
iggyi <iccyn@pm.me>
2021-10-22 15:59:05 +1100

    dapp only branch

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -18,4 +19,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: +process.env.HARDHAT_CHAIN_ID || 1337,
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
+      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
